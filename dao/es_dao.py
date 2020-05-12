7b7b376201eece10ca8e9efbdebf56ccdde42d35
susy <susy@tensynchina.com>
2020-05-12 20:25:05 +0800

    wx

--- dao/es_dao.py
+++ dao/es_dao.py
@@ -21,7 +21,9 @@ class EsConnections(object):
         if not hosts:
             hosts = ES["hosts"]
         print("elastic search hosts:", hosts)
-        self.es = Elasticsearch(hosts, sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=60,
+        # self.es = Elasticsearch(hosts, sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=60,
+        #                         sniff_timeout=10, retry_on_timeout=True)
+        self.es = Elasticsearch(hosts, sniff_on_start=False, sniff_on_connection_fail=False, sniffer_timeout=60,
                                 sniff_timeout=10, retry_on_timeout=True)
         logger.info("EsConnections es init hosts=%s" % hosts)
         print("elastic search es:", self.es)
