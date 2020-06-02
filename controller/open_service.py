a8a4f60c1ed041e43143f6c9788f4af29f82593e
susy <susy@tensynchina.com>
2020-06-03 00:28:41 +0800

    man manage

--- controller/open_service.py
+++ controller/open_service.py
@@ -132,7 +132,7 @@ class OpenService(BaseService):
 
         return None
 
-    def search(self, path_tag, tag, keyword, source, pid, page, size=50, pos=2):
+    def search(self, path_tag, tag, keyword, source, pid, page, size=50, lvl_pos=2):
         _app_map_cache = {}
         if not source:
             source = 'shared'
@@ -188,8 +188,8 @@ class OpenService(BaseService):
                     _sort_fields = [{"filename": {"order": "asc"}}]
                 else:
                     if not path_tag and not kw:
-                        if pos and pos > 0:
-                            sp.add_must(field='pos', value=pos)
+                        if lvl_pos and lvl_pos > 0:
+                            sp.add_must(field='pos', value=lvl_pos)
                         _sort_fields = [{"parent": {"order": "desc"}}, {"filename": {"order": "asc"}}]
                     else:
                         _sort_fields = [{"pos": {"order": "asc"}}, {"filename": {"order": "asc"}}]
