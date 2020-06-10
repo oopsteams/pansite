2ba65e655e6572b378872332305e065e981b2e73
susy <susy@tensynchina.com>
2020-06-10 13:16:29 +0800

    cache bug

--- utils/caches.py
+++ utils/caches.py
@@ -80,13 +80,13 @@ def clear_cache(key):
     find_target = False
     for idx in range(_l, 0, -1):
         data_obj = DATA_CACHES_TIMEOUT_KEYS_INDEX[idx - 1]
-        _key = data_obj.get('key', '')
-        if _key == key:
-            log.debug("find target keys_index:{}, index:{}".format(data_obj, idx-1))
+        _key = data_obj.get('key', None)
+        if _key and _key == key:
+            log.debug("clear_cache find target keys_index:{}, index:{}".format(data_obj, idx-1))
             find_target = True
             break
     if find_target and idx > 0:
-        log.warn("clear cache find [{}][index:{}], will remove it!".format(key, idx-1))
+        log.warn("clear cache by find [{}][index:{}], will remove it!".format(key, idx-1))
         keys_index = DATA_CACHES_TIMEOUT_KEYS_INDEX.pop(idx - 1)
         log.debug("keys_index:{}".format(keys_index))
         DATA_CACHES.pop(key)
