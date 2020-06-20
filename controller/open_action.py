6cf7908f485747be7cfbdb86db72ff5f502c7057
susy <susy@tensynchina.com>
2020-06-20 21:11:04 +0800

    can not force login.

--- controller/open_action.py
+++ controller/open_action.py
@@ -5,14 +5,36 @@ Created by susy at 2019/12/16
 from controller.action import BaseHandler
 from dao.community_dao import CommunityDao
 from controller.open_service import open_service
+from controller.service import pan_service
 from cfg import DEFAULT_CONTACT_QR_URI, bd_auth_path, PAN_SERVICE
-from utils import log as logger, url_decode
+from utils import log as logger, url_decode, decrypt_id, constant
 
 
 class OpenHandler(BaseHandler):
 
     def post(self):
         self.get()
+
+    def load_file_list(self, source, node_id, page, size):
+        params = []
+        meta = {
+            "has_next": False, "pagesize": size
+        }
+        if not '#' == node_id:
+            if "free" == source:
+                if 'free_0' == node_id:
+                    params = pan_service.query_root_list()
+            else:
+                node_id_val = decrypt_id(node_id)
+                parent_id = int(node_id_val)
+                params, meta = pan_service.query_file_list(parent_id, int(page), int(size))
+        else:
+            # params = pan_service.query_root_list(self.request.user_id)
+            params.append({"id": "free_0", "text": constant.PAN_TREE_TXT['free_root'], "data": {"source": "free"},
+                           "children": True, "icon": "folder"})
+        if "total" not in meta:
+            meta["total"] = len(params)
+        return params, meta
 
     def get(self):
         path = self.request.path
@@ -67,6 +89,15 @@ class OpenHandler(BaseHandler):
             self.to_write_json(rs)
         elif path.endswith("/update_tags"):
             rs = open_service.sync_tags()
+            self.to_write_json(rs)
+        elif path.endswith("/wxfload"):
+            source = self.get_argument("source", "")
+            node_id = self.get_argument("id")
+            page = self.get_argument("page", "0")
+            size = self.get_argument("size", "100")
+            params, meta = self.load_file_list(source, node_id, page, size)
+            rs = meta
+            rs['data'] = params
             self.to_write_json(rs)
         else:
             self.to_write_json({})
