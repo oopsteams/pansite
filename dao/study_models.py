218351c2a9244786f3b6792214cbf663e8e8b710
susy <susy@tensynchina.com>
2020-06-28 20:42:35 +0800

    study book

--- dao/study_models.py
+++ dao/study_models.py
@@ -7,6 +7,27 @@ from dao.base import db, BaseModel
 from utils import object_to_dict
 
 
+class StudyBook(BaseModel):
+    id = AutoField()
+    account_id = IntegerField(null=False, index=True)
+    ref_id = IntegerField(null=False, index=True)
+    price = IntegerField(null=False, default=0)  # 价格/评分，分
+    name = CharField(max_length=64, null=False)
+    code = CharField(max_length=64, null=False, index=True)
+    cover = CharField(max_length=64, null=False)
+    unziped = IntegerField(null=False, default=0)
+    idx = IntegerField(null=False, default=0, index=True)
+    pin = IntegerField(null=False, default=0, index=True)
+
+    @classmethod
+    def field_names(cls):
+        return ["id", "code", "account_id", "ref_id", "price", "name", "unziped", "idx", "cover", "pin"]
+
+    @classmethod
+    def to_dict(cls, instance, excludes=[]):
+        return object_to_dict(instance, cls.field_names(), excludes)
+
+
 class StudyProps(Model):
     wx_id = IntegerField(null=False, default=0)
     code = CharField(max_length=16, null=False, index=True)
