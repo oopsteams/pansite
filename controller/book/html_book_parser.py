597076734e3752c1fc7ba18dbbdb36b25153971a
susy <susy@tensynchina.com>
2020-07-01 21:24:13 +0800

    trans pinyin

--- controller/book/html_book_parser.py
+++ controller/book/html_book_parser.py
@@ -12,8 +12,10 @@ class HTMLBookParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.data = dict()
+        self.current_tag = None
 
     def handle_starttag(self, tag, attrs):
+        self.current_tag = tag
         pass
 
     def handle_endtag(self, tag):
@@ -24,11 +26,17 @@ class HTMLBookParser(HTMLParser):
 
     def handle_data(self, data):
         if data:
+            find_chinese = False
             _l = len(data)
             for i in range(_l):
                 w = data[i]
-                if w not in self.data and is_chinese(w):
-                    self.data[w] = pinyin(w, style=Style.TONE, heteronym=True, errors="ignore")
+                if is_chinese(w):
+                    find_chinese = True
+                    break
+            if find_chinese:
+                if self.current_tag not in self.data:
+                    self.data[self.current_tag] = []
+                self.data[self.current_tag].append(lazy_pinyin(data, style=Style.TONE, errors="ignore"))
 
     def handle_comment(self, data):
         pass
