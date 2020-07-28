0bcccae42e9ff1a68292179a4e25929bee0b0399
susy <susy@tensynchina.com>
2020-07-28 14:48:47 +0800

    book shelf es interface

--- controller/open_service.py
+++ controller/open_service.py
@@ -421,7 +421,7 @@ class OpenService(BaseService):
                 for item in items:
                     if item["href"] in _items_map:
                         if titlepage_need_cover:
-                            text: Element = nElem.getElementsByTagName("text")
+                            text: Element = titlepage_node.getElementsByTagName("text")
                             print("text data:", text.childNodes[0].data)
                             titlepage_node.setAttribute("src", item["href"])
                         else:
