88e1cd590abb21aca1fbfa4f8e6f487669441837
susy <susy@tensynchina.com>
2020-07-28 15:05:53 +0800

    book shelf es interface

--- controller/open_service.py
+++ controller/open_service.py
@@ -743,8 +743,8 @@ class OpenService(BaseService):
                         sb_dict[k] = params[k]
                     updated.append(params)
                     #
-                    # StudyDao.update_books_by_id(params, sb.id)
-                    # self.sync_to_es([sb_dict])
+                    StudyDao.update_books_by_id(params, sb.id)
+                    self.sync_to_es([sb_dict])
 
         StudyDao.check_ziped_books(0, 1, callback=deal_unzip_epub)
         if updated:
