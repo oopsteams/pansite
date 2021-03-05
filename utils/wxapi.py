97c7a3a2e2d14fa46560447c530306bf648586f1
susy <susy@tensynchina.com>
2021-03-05 16:04:03 +0800

    essay

--- utils/wxapi.py
+++ utils/wxapi.py
@@ -54,7 +54,7 @@ def getkflist(access_token):
 def gen_mini_qrcode(access_token, page_path, fuzzy_id, width=280):
     params = dict(
         page=page_path,
-        sence="tag={}".format(fuzzy_id),
+        scene="tag={}".format(fuzzy_id),
         width=width
     )
     headers = {"Content-Type": ""}
